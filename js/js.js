633ddacc7ef15dd4cf6fef22b47ef7cbbc483f5c
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-09-03 14:55:28 +0200

    All files added from my computer to GitHub

---
+++ js/js.js
@@ -0,0 +1,11 @@
+/**
+ * file: js/js.js
+ * purpose: Behaviors
+ **/
+console.log("Success: JavaScript from js/js.js running!");
+console.log("This is working!");
+
+function orderSuccess(button) {
+  button.innerHTML = "Order success";
+  button.disabled = true;
+}
